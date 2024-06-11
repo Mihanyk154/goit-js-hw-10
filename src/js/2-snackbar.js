7b30d8caadf1c5de7cb982fa3040ac0c8e5998ef
Mihanyk154 <mihanyk101194@gmail.com>
2024-06-11 19:03:40 +0200

    HomeWork10

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,34 @@
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+document.querySelector('.form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const delay = parseInt(document.querySelector('input[name="delay"]').value);
+    const state = document.querySelector('input[name="state"]:checked').value;
+
+    createPromise(delay, state)
+        .then((delay) => {
+            iziToast.success({
+                message: `✅ Fulfilled promise in ${delay}ms`,
+            });
+        })
+        .catch((delay) => {
+            iziToast.error({
+                message: `❌ Rejected promise in ${delay}ms`,
+            });
+        });
+});
+
+function createPromise(delay, state) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === 'fulfilled') {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    });
+}
